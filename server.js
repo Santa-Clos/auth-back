9625ecce547670f5cbc65e8b7f79448d6aa3ed4f
Santa-Clos <xavi.arcosde@gmail.com>
2026-02-06 23:55:01 +0100

    Server.js listening on PORT env variable

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ import cookieParser from 'cookie-parser'
 import 'dotenv/config'
 
 const app = express()
+
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`server listening in http://localhost:${process.env.PORT}`)
+})
